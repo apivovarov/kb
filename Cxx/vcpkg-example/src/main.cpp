3909e4b6daee9b12bea6ff933a2c300595ab8fc9
Alexander Pivovarov <apivovarov@gmail.com>
2023-06-27 14:29:34 -0700

    Update vcpkg main.cpp

--- Cxx/vcpkg-example/src/main.cpp
+++ Cxx/vcpkg-example/src/main.cpp
@@ -5,6 +5,6 @@ int main(int, char**) {
     spdlog::info("Welcome to spdlog!");
     spdlog::error("Some error message with arg: {}", 1);
     spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::info("Support for floats {:03.2f}, {}", 1.23456, 12);
+    spdlog::info("Support for floats and bools {0:.2f}, {1:}", 1.23456, true);
     return 0;
 }
