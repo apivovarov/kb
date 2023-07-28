7a86a639d7a8ab9a8e0f46e32f052fa8c3e28bf9
Alexander Pivovarov <apivovarov@gmail.com>
2023-07-27 18:10:06 -0700

    Add inference.py to roberta-large

---
+++ Sagemaker/hf/roberta-large/code/inference.py
@@ -0,0 +1,13 @@
+import logging
+import torch
+import json
+import numpy as np
+
+def input_fn(input_data, content_type):
+    logging.debug(f"Run input_fn. {input_data=}, {content_type=}")
+    assert content_type == 'application/json', "Unexpected {content_type=}. Need application/json"
+    data = json.loads(input_data)
+    np_array = np.array(data, dtype="int64")
+    tensor = torch.LongTensor(np_array).cuda()
+    logging.info(f"input tensor dtype: {tensor.dtype=}, shape: {tensor.shape=}")
+    return tensor
