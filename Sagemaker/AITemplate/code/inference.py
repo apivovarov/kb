0300a0f83b68213e5c022fcb9baeb54a3fb5cd78
Alexander Pivovarov <apivovarov@gmail.com>
2023-08-04 13:56:29 -0700

    Add gpu_id to inference.py

--- Sagemaker/AITemplate/code/inference.py
+++ Sagemaker/AITemplate/code/inference.py
@@ -8,9 +8,7 @@ from pipeline_stable_diffusion_ait import StableDiffusionAITPipeline
 
 def process_data(data: dict) -> dict:
     assert "prompt" in data, 'Key "prompt" not found in the input json'
-    prompt_arr = data["prompt"]
-    if not isinstance(prompt_arr, list):
-        prompt_arr = [prompt_arr]
+    prompt_arr = data["prompt"] # SD pipeline supports str or List[str]
     return {
         "prompt": prompt_arr,
         "guidance_scale": data.get("guidance_scale", 7.5),
@@ -19,8 +17,9 @@ def process_data(data: dict) -> dict:
         "width": 512,
     }
 
-def model_fn(model_dir: str):
-    logging.info(f"Loading model from {model_dir=}")
+def model_fn(model_dir: str, context):
+    gpu_id = context.system_properties.get("gpu_id")
+    logging.info(f"Loading model from {model_dir=}, {gpu_id=}")
     pipe = StableDiffusionAITPipeline.from_pretrained(
         model_dir,
         scheduler=EulerDiscreteScheduler.from_pretrained(
@@ -30,9 +29,9 @@ def model_fn(model_dir: str):
         torch_dtype=torch.float16,
     )
     if torch.cuda.is_available():
-        pipe = pipe.to("cuda")
+        pipe = pipe.to(f"cuda:{gpu_id}")
 
-    logging.info(f"Model was loaded from {model_dir=}")
+    logging.info(f"Model was loaded from {model_dir=} to {gpu_id=}")
     return pipe
 
 def input_fn(input_data, content_type):
